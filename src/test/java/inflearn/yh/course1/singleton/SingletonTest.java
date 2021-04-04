e482f2ef02a3e972159bc0fdb4cc522c381e120f
giyeon95 <giyeon15@gmail.com>
2021-04-04 19:10:39 +0900

    learn spring (inflearn yh)

---
+++ src/test/java/inflearn/yh/course1/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package inflearn.yh.course1.singleton;
+
+import inflearn.yh.course1.AppConfig;
+import inflearn.yh.course1.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService1 = " + memberService1);
+
+        Assertions.assertThat(memberService).isSameAs(memberService1);
+
+    }
+
+}
