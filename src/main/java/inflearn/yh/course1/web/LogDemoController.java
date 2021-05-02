596f966a5f386d8b1c87f49e5fcfd2eb4fb1e175
giyeon95 <giyeon15@gmail.com>
2021-05-02 22:54:34 +0900

    inflearn bean scope (request)

---
+++ src/main/java/inflearn/yh/course1/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package inflearn.yh.course1.web;
+
+import inflearn.yh.course1.common.MyLogger;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @GetMapping("/log-demo")
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+
+}
