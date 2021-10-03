10d3a7607c3cbab4fae0e1b94e597f1e0316d1cc
giyeon95 <giyeon15@gmail.com>
2021-10-03 19:38:16 +0900

    validation v1 by yh

--- inflearn-course2.2/src/main/java/yh/inflearn/itemservice/web/basic/BasicItemController.java
+++ inflearn-course2.2/src/main/java/yh/inflearn/itemservice/web/basic/BasicItemController.java
@@ -1,13 +1,16 @@
 package yh.inflearn.itemservice.web.basic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +23,7 @@ import yh.inflearn.itemservice.domain.item.ItemRepository;
 import yh.inflearn.itemservice.domain.item.ItemType;
 import yh.inflearn.itemservice.domain.item.ItemUpdateDTO;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/basic/items")
@@ -69,12 +73,48 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
-        Item savedItem = itemRepository.save(item);
+    public String save(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes, Model model) {
 
+        Map<String, String> errors = new HashMap<>();
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수 입니다.");
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1_000_000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 복합룰 적용
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10_000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        // 검증 실패시 입력 폼으로 이동
+        if(isNotEmpty(errors)) {
+            log.info("errors = {}", errors);
+
+            model.addAttribute("errors", errors);
+            return "basic/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("isSave", true); // ?isSave=true
         return "redirect:/basic/items/{itemId}";
+    }
+
+    private boolean isNotEmpty(Map<String, String> errors) {
+        return !errors.isEmpty();
     }
 
     @GetMapping("/add")
