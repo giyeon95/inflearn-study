241b6686389b651ab7bcddce9bcf28522c1cd192
giyeon95 <giyeon15@gmail.com>
2022-03-29 01:13:24 +0900

    inflearn spring study DispatcherType

---
+++ exception/src/main/java/hello/exception/exception/config/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.exception.exception.config;
+
+import hello.exception.exception.filter.LogFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<Filter> loggingFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+
+        return filterRegistrationBean;
+    }
+}
