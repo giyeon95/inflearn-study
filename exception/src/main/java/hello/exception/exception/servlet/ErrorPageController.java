b81aedff1367cfaca9aa12cc1ee43aead0115c74
giyeon95 <giyeon15@gmail.com>
2022-04-01 23:18:43 +0900

    inflearn spring study produces api error

--- exception/src/main/java/hello/exception/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/exception/servlet/ErrorPageController.java
@@ -1,10 +1,17 @@
 package hello.exception.exception.servlet;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -16,7 +23,25 @@ public class ErrorPageController {
         return "error-page/404";
     }
 
-    @RequestMapping("/error-page/500")
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) {
+
+        Exception ex = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        log.info("API errorPage 500");
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(Map.of(
+            "status", statusCode,
+            "message", ex.getMessage()),
+            HttpStatus.valueOf(statusCode)
+        );
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.TEXT_HTML_VALUE)
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
         return "error-page/500";
