b631c81c64fff90db3ac61fe2a756bc5fd53d336
giyeon95 <giyeon15@gmail.com>
2022-04-01 22:00:03 +0900

    inflearn spring study Interceptor

--- exception/src/main/java/hello/exception/exception/config/WebConfig.java
+++ exception/src/main/java/hello/exception/exception/config/WebConfig.java
@@ -1,19 +1,28 @@
 package hello.exception.exception.config;
 
 import hello.exception.exception.filter.LogFilter;
+import hello.exception.exception.interceptor.LogInterceptor;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean<Filter> loggingFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
